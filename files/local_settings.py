4798cc3f9d8d34918731cf16b7655dabfc025029
Pranav Gandhi <pgandhi@hubspot.com>
2013-08-11 01:20:59 -0400

    Changed local_settings.py option for sqlite3's database location.
    This was causing the db file to be placed in /var/lib instead of /opt/graphite/storage/graphite.db so I changed it to the proper place. This caused the Exec['init-db'] to run on every puppet run. The effect of this change should stop that.

--- files/local_settings.py
+++ files/local_settings.py
@@ -43,7 +43,7 @@ TIME_ZONE = 'UTC'
 DATABASES = {
    'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/var/lib/graphite/db.sqlite3',                      # Or path to database file if using sqlite3.
+        'NAME': '/opt/graphite/storage/graphite.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
